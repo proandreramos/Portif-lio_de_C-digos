2643a3ea68319485930552b148bd48dd09a5335d
proandreramos <proandreramos@gmail.com>
2024-09-13 08:34:41 -0300

    Add files via upload
    Estes arquivos se referem a exercícios propostos em cursos tanto em vídeo quanto em livros.

---
+++ Ex_001_Nome e Boas Vindas.py
@@ -0,0 +1,14 @@
+Exercício 001: Faça um programa que leia o nome da pessoa e mostre uma mensagem de boas vindas
+
+nome = input('Digite o seu nome')
+print('Prazer em conhecer,{}'.format(nome))
+
+Digite o seu nome André
+Prazer em conhecer,André
+nome = input('Digite o seu nome:')
+print('É um prazer te conhecer,',nome)
+
+#Aqui foi usado virgula logo após a sequência de string
+
+Digite o seu nome: Jonas
+É um prazer te conhecer,  Jonas
---
+++ Ex_003_Somar Números.py
@@ -0,0 +1,11 @@
+Exercício 3 : Crie um programa que leia dois números e mostre a soma entre eles
+
+n1 = int(input('Digite o 1º número:'))
+n2 = int(input('Digite o 2º número:'))
+total = n1 + n2
+print('O resultado da soma entre {} e {} é igual à {}'.format(n1,n2,total))
+
+Digite o 1º número: 5
+Digite o 2º número: 4
+
+O resultado da soma entre 5 e 4 é igual à 9
